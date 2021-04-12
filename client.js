b0ad6525174862af0ce600cfbf4d952305cc41a3
John Turner <johnturner4004@gmail.com>
2021-04-12 14:32:17 -0400

    this will add buttons and functions necessary for assignment

---
+++ client.js
@@ -0,0 +1,30 @@
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('JQ');
+  $('#generate').on('click', generateDiv);
+  $('#target').on('click', '.yellowButton', makeYellow);
+  $('#target').on('click', '.deleteButton', deleteDiv);
+}
+
+let count = 0;
+
+function generateDiv() {
+  console.log('click');
+  count++
+  $('#target').append(`
+    <div class="color">
+    <button class="yellowButton">Yellow</button>
+    <button class="deleteButton">Delete</button>
+    <p class="count">${count}</p>
+    </div>
+  `);
+}
+
+function makeYellow() {
+  $(this).parent().addClass('yellow');
+}
+
+function deleteDiv() {
+  $(this).parent().remove();
+}
